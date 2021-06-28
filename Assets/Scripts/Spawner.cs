9814a0e89d36e854acc55b008043a6391ae6a553
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-06-29 02:03:21 +0300

    Create and fill spawner script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    public float minValue;
+    public float maxValue;
+
+    public void Spawn()
+    {
+        float newX = Random.Range(minValue, maxValue);
+        Vector2 newPos = new Vector2(newX, transform.position.y);
+        Enemy enemy = Instantiate(enemyPrefab, newPos, Quaternion.identity).GetComponent<Enemy>();
+    }
+}
