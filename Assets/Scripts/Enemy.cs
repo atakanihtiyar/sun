59275c2b7e2816d2a92aa227cc73f006af601ead
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-07 19:42:11 +0300

    Add patrol script and remove movement in enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,13 +11,13 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        goal = GameObject.FindGameObjectWithTag("Player").transform;
+        //goal = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     void FixedUpdate()
     {
-        Vector3 diff = goal.position - transform.position;
-        rigidBody.velocity = diff.normalized * speedMultiplier;
+        //Vector3 diff = goal.position - transform.position;
+        //rigidBody.velocity = diff.normalized * speedMultiplier;
     }
 
     public void DestroyMe()
