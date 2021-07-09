2d71b779143a77d53a7ef45e5378db9ea977b8fb
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-10 00:35:18 +0300

    Add to dos and instantiate objects when health equal 0

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
+    public List<UnityEvent> toDosWhenIDie;
+    public List<GameObject> SpawnObjectsWhenIDie;
+
     public float health;
 
     public void DecreaseHealth()
     {
         --health;
         if (health <= 0)
+        {
+            toDosWhenIDie.ForEach(toDo => { toDo?.Invoke(); });
+            SpawnObjectsWhenIDie.ForEach(spawnObject => 
+            { 
+                StraightMovement straightMovement = Instantiate(spawnObject, transform.position, Quaternion.identity).GetComponent<StraightMovement>();
+                straightMovement.OnCreate();
+
+            });
             Destroy(gameObject);
+        }
     }
 }
