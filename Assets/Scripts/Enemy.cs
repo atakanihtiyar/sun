fc03b723969dbcd6b1a7fd288c1df1d9895551dc
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-09 23:48:48 +0300

    Delete movement part from enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,22 +4,6 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
-    private Transform goal;
-    public float speedMultiplier;
-
-    private void Start()
-    {
-        rigidBody = GetComponent<Rigidbody2D>();
-        //goal = GameObject.FindGameObjectWithTag("Player").transform;
-    }
-
-    void FixedUpdate()
-    {
-        //Vector3 diff = goal.position - transform.position;
-        //rigidBody.velocity = diff.normalized * speedMultiplier;
-    }
-
     public void DestroyMe()
     {
         Destroy(gameObject);
