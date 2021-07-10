c4b4a44d218efc47d014a350d26dc7bcd3a683f2
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-10 17:23:48 +0300

    Not working kamikaze script

---
+++ Assets/Scripts/Movements/KamikazeMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeMovement : StraightMovement
+{
+    private int whichXWillIDie;
+    public int minXToDie;
+    public int maxXToDie;
+
+    public override void OnCreate()
+    {
+        base.OnCreate();
+
+        whichXWillIDie = transform.position.x > 0 ? Random.Range(minXToDie, maxXToDie) : -Random.Range(minXToDie, maxXToDie);
+    }
+
+    public override int NewDirectionY()
+    {
+        if ((whichXWillIDie - transform.position.x) < 0.2f)
+        {
+            Transform player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            return (int)player.position.y;
+        }
+
+        return 0;
+    }
+}
