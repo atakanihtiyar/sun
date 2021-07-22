005ca95522a5bcee2eb0b0307152227f5cc8e611
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-22 17:27:07 +0300

    Create gunholder for pick up guns from ground.

---
+++ Assets/Scripts/Guns/GunHolder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GunHolder : MonoBehaviour
+{
+    private Gun gun;
+    public int gunNr;
+    public bool pickedUp;
+
+    private void Start()
+    {
+        gun = GunContainer.GetGun(gunNr);
+    }
+
+    public Gun GetGun()
+    {
+        return gun;
+    }
+}
