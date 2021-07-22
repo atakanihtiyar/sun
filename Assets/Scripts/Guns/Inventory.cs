61bcd387b27c31d3d2b911ce91d40cb2da375eaf
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-22 17:17:28 +0300

    Create gun types, weapon (in game gameobject gun)

---
+++ Assets/Scripts/Guns/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private Gun gun;
+
+    // TODO: Bu fonksiyon upgrade'i aldığımız zaman aktifleşecek. Upgrade'e dokunma ya da upgrade'e ateş etme gibi. verdiğimiz gameobject Weapon scriptini içermeli
+    public void PickupGun(GameObject g)
+    {
+        if (g == null) return;
+        Weapon w = (Weapon)g.GetComponent(typeof(Weapon));
+        if (w.pickedUp) return;
+        w.pickedUp = true;
+
+        gun = w.GetGun();
+
+        // Belki weapon object destroy edilebilir.
+        g.SetActive(false);
+    }
+
+    public Gun GetCurrentGun()
+    {
+        return gun;
+    }
+
+    public void ResetInventory()
+    {
+        gun = null;
+    }
+}
---
+++ Assets/Scripts/Guns/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private Gun gun;
+    public int gunNr;
+    public bool pickedUp;
+
+    private void Start()
+    {
+        gun = GunContainer.GetGun(gunNr);
+    }
+
+    public Gun GetGun()
+    {
+        return gun;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse is down");
+ }
+}
