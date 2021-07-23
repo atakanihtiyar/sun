2f8255586999b7633ec7702b48cf3437d59d7077
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-24 01:27:24 +0300

    Fit collider boundaries to camera boundaries

--- Assets/Scripts/GameZone.cs
+++ Assets/Scripts/GameZone.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class GameZone : MonoBehaviour
 {
+    public Camera cam;
+    public BoxCollider2D box;
+
+    void Start()
+    {
+        if (!cam.orthographic)
+        {
+            Debug.LogError("Camera must be Orthographic.");
+            return;
+        }
+        box = GetComponent<BoxCollider2D>();
+
+        float aspect = (float)Screen.width / Screen.height;
+        float orthoSize = cam.orthographicSize;
+
+        float width = 2.0f * orthoSize * aspect;
+        float height = 2.0f * cam.orthographicSize;
+
+        box.size = new Vector2(width, height);
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
