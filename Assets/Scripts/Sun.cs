b1f7cb46758c8124535119d56e637f32752ef57f
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-23 21:13:40 +0300

    Set new sun position and manage input for vertical movement

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Sun : MonoBehaviour
 {
     private float multiplier = 50;
+    public float speed;
+
+    public float yMin;
+    public float yMax;
 
     public float minRotateAngle;
     public float maxRotateAngle;
 
+    private void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float yDiff = verticalInput * speed * Time.deltaTime;
+        if (transform.position.y + yDiff < yMax && transform.position.y + yDiff > yMin)
+            transform.Translate(0, yDiff, 0);
+    }
+    
     void FixedUpdate()
     {
         float turnInput = Input.GetAxis("Fire1");
@@ -22,7 +34,7 @@ public class Sun : MonoBehaviour
             transform.localEulerAngles = currentRotation;
         }
     }
-
+    
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
