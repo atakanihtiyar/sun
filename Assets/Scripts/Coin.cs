cd89d989b2a37ced4228a8c94a771543b1dcb786
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-07-24 14:13:20 +0300

    Add coin script and prefab

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+
+    public int coin;
+
+    private float speed = 0;
+
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        GameObject sun = GameObject.FindGameObjectWithTag("Sun");
+
+        speed += Time.fixedDeltaTime * 3;
+        rigidbody2d.velocity = (sun.transform.position - transform.position).normalized * speed;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Sun"))
+        {
+            CoinManager.OnEarnScore(coin);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour
+{
+    public delegate void ManipulateCoin(int coin);
+    public static ManipulateCoin onEarnCoin;
+
+    public static Text coinText;
+    public static int coin;
+
+    private void Start()
+    {
+        coinText = GetComponent<Text>();
+    }
+
+    public static void OnEarnScore(int coinToAdd)
+    {
+        onEarnCoin?.Invoke(coinToAdd);
+
+        coin += coinToAdd;
+        coinText.text = "" + coin;
+    }
+}
